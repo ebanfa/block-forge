fc1c4b0816096ce764c7ec8d6b82de326fefdd0a
Edward Banfa <ebanfa@gmail.com>
2024-04-09 13:49:44 +0300

    Initial implementation of BtcBlockExtractor

--- go.mod
+++ go.mod
@@ -4,7 +4,16 @@ go 1.22.0
 
 require (
 	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef // indirect
+	github.com/btcsuite/btcd v0.24.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
@@ -16,6 +25,7 @@ require (
 	go.uber.org/fx v1.21.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/etl/process/etl.go
+++ pkg/etl/process/etl.go
@@ -1,13 +1,8 @@
-// Package process provides a comprehensive API for Extract, Transform, and Load (ETL) operations
-// on blockchain data. It allows you to extract data from various blockchain sources,
-// transform it into a desired format, and load it into centralized data stores or other
-// blockchain destinations.
 package process
 
 import (
 	"github.com/edward1christian/block-forge/pkg/application/common/context"
-	"github.com/edward1christian/block-forge/pkg/application/components"
-	systemApi "github.com/edward1christian/block-forge/pkg/application/system"
+	"github.com/edward1christian/block-forge/pkg/application/system"
 	etlComponentsApi "github.com/edward1christian/block-forge/pkg/etl/components"
 )
 
@@ -15,16 +10,17 @@ import (
 type ETLProcessStatus string
 
 const (
-	ETLProcessStatusInitialized ETLProcessStatus = "initialized"
-	ETLProcessStatusRunning     ETLProcessStatus = "running"
-	ETLProcessStatusPaused      ETLProcessStatus = "paused"
-	ETLProcessStatusCompleted   ETLProcessStatus = "completed"
-	ETLProcessStatusFailed      ETLProcessStatus = "failed"
-	ETLProcessStatusStopped     ETLProcessStatus = "stopped"
+	ETLProcessStatusUnInitialized ETLProcessStatus = "Uninitialized"
+	ETLProcessStatusInitialized   ETLProcessStatus = "initialized"
+	ETLProcessStatusRunning       ETLProcessStatus = "running"
+	ETLProcessStatusPaused        ETLProcessStatus = "paused"
+	ETLProcessStatusCompleted     ETLProcessStatus = "completed"
+	ETLProcessStatusFailed        ETLProcessStatus = "failed"
+	ETLProcessStatusStopped       ETLProcessStatus = "stopped"
 )
 
-// Source represents a data source from which data can be extracted.
-type Source interface {
+// SourceInterface represents a data source from which data can be extracted.
+type SourceInterface interface {
 	// GetID returns the unique identifier of the source.
 	GetID() string
 
@@ -35,8 +31,8 @@ type Source interface {
 	GetConfig() map[string]interface{}
 }
 
-// Destination represents a destination where data can be loaded.
-type Destination interface {
+// DestinationInterface represents a destination where data can be loaded.
+type DestinationInterface interface {
 	// GetID returns the unique identifier of the destination.
 	GetID() string
 
@@ -47,8 +43,8 @@ type Destination interface {
 	GetConfig() map[string]interface{}
 }
 
-// Record represents a single data record in the ETL pipeline.
-type Record struct {
+// RecordInterface represents a single data record in the ETL pipeline.
+type RecordInterface struct {
 	// Data holds the actual data of the record.
 	Data interface{}
 
@@ -56,66 +52,47 @@ type Record struct {
 	Metadata map[string]interface{}
 }
 
-// ETLProcessComponent is an component that belongs to an ETL process
-type ETLProcessComponent interface {
-	// ID returns the unique identifier of the component.
-	ID() string
-
-	// Name returns the name of the component.
-	Name() string
-
-	// Type returns the type of the component.
-	Type() components.ComponentType
-
-	// Description returns the description of the component.
-	Description() string
-
-	// Initialize initializes the module.
-	// Returns an error if the initialization fails.
-	Initialize(ctx *context.Context, system systemApi.SystemInterface) error
-
-	GetProcessID() string // Gets the ID of the ETL process the component belongs to
+// ETLProcessComponentInterface is a component that belongs to an ETL process.
+type ETLProcessComponentInterface interface {
+	system.BaseSystemService
 }
 
-// Extractor defines the interface for extracting data from a source.
-type Extractor interface {
-	ETLProcessComponent
+// ExtractorInterface defines the interface for extracting data from a source.
+type ExtractorInterface interface {
 	// Extract retrieves data from the specified source and returns it as a slice of Records.
-	Extract(ctx *context.Context, source Source) ([]Record, error)
+	Extract(ctx *context.Context, source SourceInterface) ([]RecordInterface, error)
 }
 
 // StreamingExtractor defines the interface for extracting data from a source in a streaming manner.
-type StreamingExtractor interface {
-	Extractor
+type StreamingExtractorInterface interface {
+	ExtractorInterface
 
 	// OpenStream establishes a streaming connection with the data source and returns a channel
 	// for receiving records as they become available.
-	OpenStream(ctx context.Context, source Source) (<-chan Record, error)
+	OpenStream(ctx context.Context, source SourceInterface) (<-chan RecordInterface, error)
 }
 
 // ScheduledExtractor defines the interface for extracting data from a source on a scheduled basis.
 type ScheduledExtractor interface {
-	Extractor
+	ExtractorInterface
 
 	// ScheduleExtraction schedules a periodic extraction from the data source based on the provided schedule.
-	ScheduleExtraction(ctx context.Context, source Source, schedule Schedule) error
+	ScheduleExtraction(ctx context.Context, source SourceInterface, schedule Schedule) error
 
 	// ExtractionsChannel returns the channel from which scheduled extractions can be read.
-	ExtractionsChannel() <-chan []Record
+	ExtractionsChannel() <-chan []RecordInterface
 }
 
-// Transformer defines the interface for transforming data.
-type Transformer interface {
-	ETLProcessComponent
+// TransformerInterface defines the interface for transforming data.
+type TransformerInterface interface {
 	// Transform applies transformations to the input data and returns the transformed Records.
-	Transform(ctx *context.Context, records []Record) ([]Record, error)
+	Transform(ctx *context.Context, records []RecordInterface) ([]RecordInterface, error)
 }
 
-// Loader defines the interface for loading data into a destination.
-type Loader interface {
-	ETLProcessComponent
+// LoaderInterface defines the interface for loading data into a destination.
+type LoaderInterface interface {
 	// Load writes the provided Records to the specified destination.
-	Load(ctx *context.Context, destination Destination, records []Record) error
+	Load(ctx *context.Context, destination DestinationInterface, records []RecordInterface) error
 }
 
 // ETLProcess represents an individual ETL process.
