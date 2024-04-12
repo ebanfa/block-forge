bc0a733fae5a7a93d527251dcff02c1182c48496
Edward Banfa <ebanfa@gmail.com>
2024-04-12 23:41:32 +0300

    Modified store interface

--- pkg/davail/storage/store.go
+++ pkg/davail/storage/store.go
@@ -1,41 +1,33 @@
 package storage
 
-// Storage is an interface that defines the operations for storing and retrieving
-// encoded data segments, as well as repairing corrupted or missing segments.
-type Storage interface {
-	// Store takes the encoded data segments and parity segments and persistently
-	// stores them, returning a unique identifier for the stored data.
-	// The `options` parameter allows passing additional configuration options to the storage.
-	Store(dataSegments, paritySegments [][]byte, options ...StorageOption) (storageID string, err error)
-
-	// Retrieve takes the unique identifier of the stored data and retrieves the
-	// encoded data segments and parity segments.
-	// The `options` parameter allows passing additional configuration options to the retrieval.
-	Retrieve(storageID string, options ...RetrieveOption) (dataSegments, paritySegments [][]byte, err error)
-
-	// Repair takes the unique identifier of the stored data and repairs any
-	// corrupted or missing segments, returning the fully recovered encoded data.
-	// The `options` parameter allows passing additional configuration options to the repair process.
-	Repair(storageID string, options ...RepairOption) (dataSegments, paritySegments [][]byte, err error)
-
-	// Delete removes the stored data with the given unique identifier from the
-	// storage system.
-	// The `options` parameter allows passing additional configuration options to the deletion.
-	Delete(storageID string, options ...DeleteOption) error
-
-	// SetOptions allows setting global options for the storage, which will be used
-	// in all subsequent calls to Store, Retrieve, Repair, and Delete.
-	SetOptions(options ...StorageOption)
+// StorageConfig is a struct to hold configuration options for the Storage.
+type StorageConfig struct {
+	// Add storage configuration options here as needed
 }
 
 // StorageOption is a function type that can be used to set options for the Storage.
 type StorageOption func(*StorageConfig)
 
-// RetrieveOption is a function type that can be used to set options for the Retrieve operation.
-type RetrieveOption func(*RetrieveConfig)
+// Storage is an interface that defines the operations for storing and retrieving
+// encoded data segments, as well as repairing corrupted or missing segments.
+type StorageInterface interface {
+	// Store takes the encoded data segments and parity segments and persistently
+	// stores them, returning a unique identifier for the stored data.
+	Store(dataSegments, paritySegments [][]byte) (storageID string, err error)
 
-// RepairOption is a function type that can be used to set options for the Repair operation.
-type RepairOption func(*RepairConfig)
+	// Retrieve takes the unique identifier of the stored data and retrieves the
+	// encoded data segments and parity segments.
+	Retrieve(storageID string) (dataSegments, paritySegments [][]byte, err error)
 
-// DeleteOption is a function type that can be used to set options for the Delete operation.
-type DeleteOption func(*DeleteConfig)
+	// Repair takes the unique identifier of the stored data and repairs any
+	// corrupted or missing segments, returning the fully recovered encoded data.
+	Repair(storageID string) (dataSegments, paritySegments [][]byte, err error)
+
+	// Delete removes the stored data with the given unique identifier from the
+	// storage system.
+	Delete(storageID string) error
+
+	// SetOptions allows setting global options for the storage, which will be used
+	// in all subsequent calls to Store, Retrieve, Repair, and Delete.
+	SetOptions(options ...StorageOption)
+}
